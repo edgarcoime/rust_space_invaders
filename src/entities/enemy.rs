433088e24564e324309bf24717655dfdabea782c
Edgar Coime <e.coimeii@gmail.com>
2022-03-07 19:34:24 -0800

    feat: implement enemy spawn

--- src/entities/enemy.rs
+++ src/entities/enemy.rs
@@ -1,4 +1,7 @@
 use bevy::prelude::*;
+use rand::{thread_rng, Rng};
+
+use crate::{Game, WinSize, SpriteInfos};
 
 #[derive(Component)]
 pub struct FromEnemy;
@@ -11,5 +14,40 @@ pub struct EnemyState;
 
 pub struct EnemyPlugin;
 impl Plugin for EnemyPlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.
+            add_system(enemy_spawn) // Change to spawn per level?
+        ;
+    }
+}
+
+fn enemy_spawn(
+    mut commands: Commands,
+    mut game: ResMut<Game>,
+    win_size: Res<WinSize>,
+    sprite_infos: Res<SpriteInfos>,
+) {
+    if game.active_enemies < 2 {
+        // TODO: get random position?
+        // compute the rando position
+        let mut rng = thread_rng();
+        let w_span = win_size.w / 2. - 100.;
+        let h_span = win_size.h / 2. - 100.;
+        let x = rng.gen_range(-w_span..w_span) as f32;
+        let y = rng.gen_range(-h_span..h_span) as f32;
+
+        // spawn enemy
+        commands
+            .spawn_bundle(SpriteBundle {
+                texture: sprite_infos.red_enemy.0.clone(),
+                transform: Transform {
+                    translation: Vec3::new(x, y, 10.),
+                    ..Default::default()
+                },
+                ..Default::default()
+            })
+            .insert(Enemy);
+
+        game.active_enemies += 1;
+    }
 }
